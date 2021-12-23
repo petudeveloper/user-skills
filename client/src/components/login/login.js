19935f0cd2c149c1755bf5e4ef6dbda23ed90c31
petudeveloper <davidalvarezmazzo@gmail.com>
2021-12-23 00:56:50 -0500

    Update input fields

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -4,6 +4,8 @@ import styles from './login.module.css';
 const Login = () => {
   const [userNameReg, setUserNameReg] = useState('');
   const [passwordNameReg, setPasswordNameReg] = useState('');
+  const [userNameLogin, setUserNameLogin] = useState('');
+  const [passwordNameLogin, setPasswordNameLogin] = useState('');
 
   return (
     <div>
@@ -11,11 +13,11 @@ const Login = () => {
         <h1 className={styles.title}>Registration</h1>
         <label htmlFor="usernameReg">
           Username
-          <input type="text" id="usernameReg" />
+          <input type="text" id="usernameReg" onChange={(e) => {setUserNameReg(e.target.value)}} />
         </label>
         <label htmlFor="passwordReg">
           Password
-          <input type="text" id="passwordReg" />
+          <input type="text" id="passwordReg" onChange={(e) => {setPasswordNameReg(e.target.value)}} />
         </label>
         <button type="button">Register</button>
       </div>
@@ -23,11 +25,11 @@ const Login = () => {
         <h1 className={styles.title}>Login</h1>
         <label htmlFor="usernameLogin">
           Username
-          <input type="text" id="passwordLogin" />
+          <input type="text" id="passwordLogin" onChange={(e) => {setUserNameLogin(e.target.value)}} />
         </label>
         <label htmlFor="passwordLogin">
           Password
-          <input type="text" id="passwordLogin" />
+          <input type="text" id="passwordLogin" onChange={(e) => {setPasswordNameLogin(e.target.value)}} />
         </label>
         <button type="button">Register</button>
       </div>
