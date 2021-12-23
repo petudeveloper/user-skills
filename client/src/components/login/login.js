15cfac5822a5a07ad7717d78657d1b111909fa0f
petumazo <davidalvarezmazzo@gmail.com>
2021-12-23 03:52:20 -0500

    Add basic server

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Axios from 'axios';
 import styles from './login.module.css';
 
 const Login = () => {
@@ -7,29 +8,36 @@ const Login = () => {
   const [userNameLogin, setUserNameLogin] = useState('');
   const [passwordNameLogin, setPasswordNameLogin] = useState('');
 
+  const register = () => {
+    Axios.post('http://localhost:3001/register', {
+      username: userNameReg,
+      password: setPasswordNameReg
+    }).then((res) => console.log(res))
+  } 
+
   return (
     <div>
       <div className={styles.formContainer}>
         <h1 className={styles.title}>Registration</h1>
         <label htmlFor="usernameReg">
           Username
-          <input type="text" id="usernameReg" onChange={(e) => {setUserNameReg(e.target.value)}} />
+          <input type="text" id="usernameReg" value={userNameReg} onChange={(e) => { setUserNameReg(e.target.value); }} />
         </label>
         <label htmlFor="passwordReg">
           Password
-          <input type="text" id="passwordReg" onChange={(e) => {setPasswordNameReg(e.target.value)}} />
+          <input type="text" id="passwordReg" value={passwordNameReg} onChange={(e) => { setPasswordNameReg(e.target.value); }} />
         </label>
-        <button type="button">Register</button>
+        <button type="button" onClick={register}>Register</button>
       </div>
       <div className={styles.formContainer}>
         <h1 className={styles.title}>Login</h1>
         <label htmlFor="usernameLogin">
           Username
-          <input type="text" id="passwordLogin" onChange={(e) => {setUserNameLogin(e.target.value)}} />
+          <input type="text" id="passwordLogin" onChange={(e) => { setUserNameLogin(e.target.value); }} />
         </label>
         <label htmlFor="passwordLogin">
           Password
-          <input type="text" id="passwordLogin" onChange={(e) => {setPasswordNameLogin(e.target.value)}} />
+          <input type="text" id="passwordLogin" onChange={(e) => { setPasswordNameLogin(e.target.value); }} />
         </label>
         <button type="button">Register</button>
       </div>
--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const mysql = require("mysql");
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
   user:"root",
   host: "localhost",
-  password: "password",
-  database: "LoginSystem",
+  password: "petujesusmazo",
+  database: "users",
+  port: 3306,
 });
 
 app.post('/register', (req, res) => {
+
+  const username = req.body.username
+  const password = req.body.password
+
   db.query(
     "INSERT INTO users (username, password) VALUES (?,?)",
     [username, password], (err, result) => {
